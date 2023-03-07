a3ed260b6472edbf0fbcb0fc26d45c98c329b26f
eusebio fernandez <eusebio@MacBook-Air.home>
2023-03-07 21:40:53 +0100

    solucion del ejercicio

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,83 @@
 // Soldier
-class Soldier {}
+class Soldier {
+
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`;
+
+        } else {
+            return `${this.name} has died in act of combat`;
+        }
+    }
+
+    battleCry() {
+        return "Odin Owns You All!";
+    }
+}
+
+
+
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+    constructor(health, strength) {
+        super(health, strength);
+
+    }
+
+
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+
+        if (this.health <= 0) {
+            return "A Saxon has died in combat";
+
+        } else {
+            return `A Saxon has received ${damage} points of damage`;
+        }
+    }
+}
+
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addViking(objectViking) {
+        this.vikingArmy.push(objectViking);
+    }
+    
+    addSaxon(objectSaxon) {
+        this.saxonArmy.push(objectSaxon);
+
+    }
+
+}
